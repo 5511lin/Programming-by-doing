d05a4294251310eb31c39bf77d336d5055bc011b
5511lin <89062319+5511lin@users.noreply.github.com>
2022-01-16 11:22:51 +0800

    file name

---
+++ C++/Programming by Doing Conversion01/YourInitials.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+cout <<"For the name Julian Lin..."<<endl;
+        cout<<"                      "<<endl;
+        cout <<"JJJJJ  L"<<endl;
+        cout <<"  J    L"<<endl;
+        cout <<"  J    L"<<endl;
+        cout <<"  J    L"<<endl;
+        cout <<"J J    L "<<endl;
+        cout <<"J J    L "<<endl;
+        cout <<" JJ    LLLLLL "<<endl;
+return 0;
+
+}
