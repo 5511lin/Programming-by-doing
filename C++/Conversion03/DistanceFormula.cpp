f61e2c679f7ea827c7e49ac754883a8130505622
5511lin <89062319+5511lin@users.noreply.github.com>
2022-01-30 22:17:56 +0800

    file name

---
+++ C++/Conversion03/DistanceFormula.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+int main() {
+		// test the formula a bit
+		double d1 = (-2,1 , 1,5);
+		cout <<" (-2,1) to (1,5) => "<< + d1 <<endl;
+
+		double d2 = (-2,-3 , -4,4);
+		cout <<" (-2,-3) to (-4,4) => " + d2 <<endl;
+
+		cout <<" (2,-3) to (-1,-2) => "<< + (2,-3,-1,-2) <<endl;
+
+		cout <<" (4,5) to (4,5) => "<< + (4,5,4,5) << endl;
+	
+		// put your code up in here
+		double d = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		return d;
+	}
