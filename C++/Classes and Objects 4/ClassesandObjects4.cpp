6d18ac646aee1e1dabf4e47b5d409790c34086d4
5511lin <89062319+5511lin@users.noreply.github.com>
2022-03-13 21:40:55 +0800

    file name

--- C++/Classes and Objects 4/ClassesandObjects4.cpp
+++ C++/Classes and Objects 4/ClassesandObjects4.cpp
@@ -51,8 +51,14 @@ double distanceP(Planet a, Planet b){
 }
 
 int main(){
-    Planet solarSystem[8] = {
+    Planet solarSystem[32] = {
         Planet("Mercury", 69.592, 0.33, 4879.4, 0, Moon("NO NOON",0,0,0),Moon("NO NOON",0,0,0),Moon("NO NOON",0,0,0)),
+        Planet("Venus",4.87,12104,108.2,0,Moon("NO MOON"),Moon("NO MOON"),Moon("NO MOON")),
+        Planet("Earth",5.97,12756,149.6,1,Moon("Moon",378000,0.07346,3476.2),Moon("NO MOON"),Moon("NO MOON")),
+        Planet("Mars",0.642,6792,227.9,2,Moon("Phobos",9270,22.5,10.6),Moon("Deimos", 23460,12.4,2.4),Moon("NO MOON")),
+        Planet("Saturn",568,120536,1433.5,82,Moon("Titan",1221850,5150,1345.5),Moon("Mimas",185520,398,0.379),Moon("Rhea",527040,1528,23.1)),
+        Planet("Uranus",86.8,51118,2872.5,27,Moon("Umbriel",265970,1190,12.2),Moon("Ariel",191240,1160,12.9),Moon("Miranda",129780,472,0.66)),
+        Planet("Neptune",102,49528,4495.1,14,Moon("Neso",47279670,60,0.002),Moon("Sao",22337190,38,0.001),Moon("Naiad",48200,50,0.002)),
         Planet("Jupiter", 745.39, 1998.13, 193820, 79, Moon("Io",421600,3629,893.2),Moon("Europa",670900,3126,480),Moon("Callisto",1883000,4800,1075))
     };
     cout<< "Distance area:= " <<distanceP(solarSystem[0],solarSystem[1]) <<endl;
---
+++ C++/Classes and Objects 4/Untitled-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ int Array[6]= {1, 2, 3, 4, 5, 6};
+int main(){
+    int num1 = 7, num2 = 8, num3 =9;
+    cout<< Array[0];
+    cout<< Array[1];
+    cout<< Array[2]<<endl;
+    cout<< Array[3];
+    cout<< Array[4];
+    cout<< Array[5]<<endl;
+    cout<< num1;
+    cout<< num2;
+    cout<< num3;
+}
