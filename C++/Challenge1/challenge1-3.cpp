4f5c8ef99ca33e31903664c0c6e4dff9bff16975
5511lin <89062319+5511lin@users.noreply.github.com>
2022-03-26 19:48:46 +0800

    file name

--- C++/Challenge1/challenge1-3.cpp
+++ C++/Challenge1/challenge1-3.cpp
@@ -3,23 +3,21 @@ using namespace std;
  
 int calculate_sum(int a, int B, int N){
 
-    int m = N / (a*B); 
-    int sum = m * (m + 1) / 2;
-    int ans = a * sum;
- 
-    return ans;
+    N--;
+   return (((N/a) * (1 + (N/a)) * a / 2) + ((N/B) * (1 + (N/B)) * B / 2) - ((N/(a*B)) * (1 + (N/(a*B))) * (a*B) / 2));
 }
  
 //run
 int main(){
-    int a = 0, N = 10000, B = 0;
+    int a = 0, N = 0, B = 0;
     cout << "Input A: ";
     cin >> a;
     cout << endl << "Input B: ";
     cin >> B;
-
+    cout << endl << "Input Limit N: ";
+    cin >> N;
     cout << "Sum of multiples of "
-         << a << " and " << B << " up to 100 = "
+         << a << " and " << B << " up to "<<N<<" = "
          << calculate_sum(a, B, N) << endl;
     return 0;
 }
