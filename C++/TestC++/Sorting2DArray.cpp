7e72b0c1328484a4798b115c0619cfad1933b7aa
5511lin <89062319+5511lin@users.noreply.github.com>
2022-04-10 21:51:28 +0800

    file name

---
+++ C++/TestC++/Sorting2DArray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main (){
+    int n, m;
+
+    cout << "Welcome to the Alphabet Sorter!\nEnter Size (enter 0 0 to exit):"<<endl;
+    cin >> n; cin >> m; 
+
+    char arr[n][m];
+    //char alph [] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+    int c;
+    int r;
+
+
+
+     cout << "Generated Array: " << endl;
+    srand (time(NULL));
+    for (int i=0;  i < n; i++) {    
+        for (int j = 0; j < m; j++){
+            r = rand() % 26;
+            c = 'a' + r; 
+            arr [i][j] = (char)c;
+            cout << arr [i][j] << " ";
+            }
+        cout << endl;
+}
+}
